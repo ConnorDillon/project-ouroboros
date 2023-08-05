a548e4ae0db6b7e1f8fcf8d513698fa3ec87bfb0
Connor Dillon <connor@cdillon.nl>
2023-08-05 14:23:12 +0200

    Fix free vars in let expr

--- src/ast.rs
+++ src/ast.rs
@@ -35,10 +35,24 @@ impl AST<Fun> {
                 hs
             }
             AST::Fn(f) => f.free_vars(),
-            AST::Let(_, bs, e) => {
-                let fvs: HashSet<String> =
-                    bs.iter().map(|(_, e)| e.free_vars()).flatten().collect();
-                fvs.union(&e.free_vars()).cloned().collect()
+            AST::Let(r, bs, e) => {
+                let mut bound: HashSet<String> = match r {
+                    Rec::NonRec => HashSet::new(),
+                    Rec::Rec => bs.iter().map(|(v, _)| v).cloned().collect(),
+                };
+                let mut free: HashSet<String> = HashSet::new();
+                for (v, e) in bs {
+                    if *r == Rec::NonRec {
+                        bound.insert(v.clone());
+                    }
+                    for f in e.free_vars().difference(&bound) {
+                        free.insert(f.clone());
+                    }
+                }
+                for f in e.free_vars().difference(&bound) {
+                    free.insert(f.clone());
+                }
+                free
             }
             AST::Expr(es) => es.iter().map(|e| e.free_vars()).flatten().collect(),
             _ => HashSet::new(),
@@ -59,11 +73,13 @@ impl Fun {
     }
 }
 
+#[derive(Debug, PartialEq, Clone)]
 pub struct LiftedAST {
     pub funs: Vec<LiftedFun>,
     pub ast: AST<usize>,
 }
 
+#[derive(Debug, PartialEq, Clone)]
 pub struct LiftedFun {
     pub args: Vec<String>,
     pub body: AST<usize>,
@@ -90,7 +106,7 @@ fn lift(funs: &mut Vec<LiftedFun>, ast: AST<Fun>) -> AST<usize> {
         }
         AST::Expr(x) => AST::Expr(x.into_iter().map(|x| lift(funs, x)).collect()),
         AST::Let(rec, vars, expr) => AST::Let(
-	    rec,
+            rec,
             vars.into_iter().map(|(x, y)| (x, lift(funs, y))).collect(),
             Box::new(lift(funs, *expr)),
         ),
@@ -103,12 +119,36 @@ fn lift(funs: &mut Vec<LiftedFun>, ast: AST<Fun>) -> AST<usize> {
 
 #[cfg(test)]
 mod tests {
-    use crate::parser::parse;
+    use crate::{
+        ast::{LiftedAST, LiftedFun, Rec, AST},
+        parser::parse,
+    };
 
     #[test]
     fn test_free_vars() {
         assert_eq!(parse("(fn (x) x))").unwrap().free_vars().len(), 0);
         assert_eq!(parse("(fn (x) (+ x y))").unwrap().free_vars().len(), 1);
         assert_eq!(parse("(if true false nil)").unwrap().free_vars().len(), 0);
+        assert_eq!(
+            parse("(fn (x) (let ((y 1)) y))").unwrap().free_vars().len(),
+            0
+        );
+    }
+
+    #[test]
+    fn test_lift() {
+        let ast = parse("(fn (x) (let ((y 1)) y))").unwrap().lift_lambdas();
+        let lifted = LiftedAST {
+            funs: vec![LiftedFun {
+                args: vec!["x".into()],
+                body: AST::Let(
+                    Rec::NonRec,
+                    vec![("y".into(), AST::Int(1))],
+                    Box::new(AST::Symbol("y".into())),
+                ),
+            }],
+            ast: AST::Fn(0),
+        };
+        assert_eq!(ast, lifted);
     }
 }
